af929c3ca7f954980c8c2d6836919ca081690b7f
Alejandro Pérez Durán <apduran95@gmail.com>
2025-03-04 14:40:26 -0500

    initialize project structure with basic configuration and components

---
+++ android/app/src/main/java/todo/list/MainActivity.java
@@ -0,0 +1,5 @@
+package todo.list;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'todo.list',
+  appName: 'todo-list',
+  webDir: 'dist'
+};
+
+export default config;
